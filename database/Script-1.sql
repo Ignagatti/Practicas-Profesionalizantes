92a56671424cf667f8ecb9cc675021db0c785e5d
Ignagatti <gattiignacio85@gmail.com>
2026-05-19 20:12:48 -0300

    .

---
+++ database/Script-1.sql
@@ -0,0 +1,4 @@
+INSERT INTO Cliente (Nombre, Apellido, Telefono, Estado, Saldo, Cuit_Cuil, Email, Razon_Social) VALUES
+('Juan Carlos', 'Muller', '3496-451234', 'activo', 0.00, '20-34123456-9', 'juan.muller@email.com', NULL),
+('Mueblería', 'Esperanza S.A.', '3496-420011', 'activo', 150000.00, '30-55667788-4', 'ventas@mueblesesperanza.com', 'Muebles Esperanza S.A.'),
+('Anahí', 'García', '342-5112233', 'activo', -25000.00, '27-40123456-2', 'anahi.g@email.com', NULL);
